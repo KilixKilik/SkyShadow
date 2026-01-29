97a99c36f9dd078d4bc682d6967ddc8869a91138
Kerikush <kilikrik69@gmail.com>
2026-01-30 04:39:29 +0800

    фулл перпис фриды из-за ошибок

--- libsky.js
+++ libsky.js
@@ -1,41 +1,51 @@
-/* UI and Net Logic */
 // kilixkilik
 
-const TARGET_IP = "192.168.2.200";
-const TARGET_PORT = 1344;
+const ip = "192.168.2.200";
+const p = 1344;
 
-const _m = [83, 107, 121, 83, 104, 97, 100, 111, 119, 32, 67, 114, 101, 97, 116, 101, 32, 98, 121, 32, 107, 105, 108, 105, 120, 107, 105, 108, 105, 107, 46];
-
-function showInfo() {
-    Java.perform(() => {
-        const Toast = Java.use("android.widget.Toast");
-        const App = Java.use("android.app.ActivityThread").currentApplication();
-        const context = App.getApplicationContext();
-        const msg = String.fromCharCode.apply(null, _m);
-
-        Java.scheduleOnMainThread(() => {
-            Toast.makeText(context, msg, 1).show();
+const t_l = [
+    "nekkimobile.ru", "shadowfight2.com", "nekki.com",
+    "appsflyer.com", "facebook.com", "tapjoy.com",
+    "95.213.140.21", "51.195.27.140", "51.195.27.146"
+];
+=
+function si() {
+    setTimeout(() => {
+        Java.perform(() => {
+            const T = Java.use("android.widget.Toast");
+            const AT = Java.use("android.app.ActivityThread");
+            const ctx = AT.currentApplication().getApplicationContext();
+            Java.scheduleOnMainThread(() => {
+                T.makeText(ctx, "SkyShadow Create by kilixkilik.", 1).show();
+            });
         });
-    });
+    }, 5000);
 }
 
-const connect_ptr = Module.findExportByName("libc.so", "connect");
-
-Interceptor.attach(connect_ptr, {
-    onEnter: function (args) {
-        const sockaddr_p = args[1];
-        if (sockaddr_p.readU16() === 2) {
-            const port = sockaddr_p.add(2).readU16();
-            if (port === 20480 || port === 47873) { // 80, 443
-                sockaddr_p.add(2).writeU16((TARGET_PORT << 8) | (TARGET_PORT >> 8));
-                const p = TARGET_IP.split('.').map(Number);
-                const newAddr = (p[3] << 24) | (p[2] << 16) | (p[1] << 8) | p[0];
-                sockaddr_p.add(4).writeU32(newAddr);
-            }
+const gai = Module.findExportByName("libc.so", "getaddrinfo");
+Interceptor.attach(gai, {
+    onEnter: function (a) {
+        this.n = a[0].readUtf8String(); // get hostname
+    },
+    onLeave: function (r) {
+        if (this.n && t_l.some(t => this.n.includes(t))) {
         }
     }
 });
 
-showInfo();
+const cp = Module.findExportByName("libc.so", "connect");
+Interceptor.attach(cp, {
+    onEnter: function (a) {
+        const sa = a[1]; // sockaddr
+        if (sa.readU16() === 2) { // AF_INET
+            const pr = sa.add(2).readU16();
+            // Redirect all targets
+            sa.add(2).writeU16((p << 8) | (p >> 8)); // port
+            const pt = ip.split('.').map(Number);
+            const na = (pt[3] << 24) | (pt[2] << 16) | (pt[1] << 8) | pt[0];
+            sa.add(4).writeU32(na); // ip
+        }
+    }
+});
 
-// kilixkilik
+si(); // start toast
